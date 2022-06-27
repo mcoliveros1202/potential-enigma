9a629332a3796e2f1dd38355a6680cc98c0dae32
moliveros <mcoliveros1202@gmail.com>
2022-06-27 14:16:06 -0500

    created command line input capture functionality

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,28 @@
 // TODO: Include packages needed for this application
+const profileDataArgs = process.argv.slice(2, process.argv.length);
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
+
+const printReadmeData = profileDataArr => {
+    // This...
+    for (let i = 0; i < profileDataArr.length; i += 1) {
+        console.log(profileDataArr[i]);
+    }
+
+    console.log('==================')
+
+    // Is the same as this...
+    profileDataArr.forEach(profileItem => console.log(profileItem));
+};
+
+printReadmeData(profileDataArgs);

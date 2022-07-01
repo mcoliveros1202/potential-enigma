60db4dab8665f79c148ab2554f4a7cee91cfd971
moliveros <mcoliveros1202@gmail.com>
2022-06-30 22:15:18 -0500

    cleaned up code and retest after deletions

--- Develop/index.js
+++ Develop/index.js
@@ -1,101 +1,61 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
-const generateFile = require('./src/readme-template.js');
-const generateMarkdown = require('./utils/generateMarkdown')
-const profileDataArgs = process.argv.slice(2);
-const path = require('path')
-const [project, github] = profileDataArgs;
 
-// TODO: Create an array of questions for user input
+const generateMarkdown = require('./utils/generateMarkdown')
+
+const path = require('path')
+
+
+// Questions array
 
 const questions =[
 
     {
         type: 'input',
         name: 'project',
-        message: 'What is the title of your project? (Required)',
-        /*         validate: projectInput () => {
-                    if (projectInput) {
-                        return true;
-                    } else {
-                        console.log('Please enter the project title!');
-                        return false;
-                    }
-                } */
+        message: 'What is the title of your project?',
     },
     {
         type: 'input',
         name: 'description',
-        message: 'Please provide a brief description of your project. (Required)',
-        /*     validate: descriptionInput => {
-                if (descriptionInput) {
-                    return true;
-                } else {
-                    console.log("Please provide a brief description of your project!");
-                    return false;
-                }
-            } */
+        message: 'Please provide a brief description of your project.',
     },
     {
         type: 'input',
         name: 'installation',
-        message: 'Please provide steps necessary to install your project. (Required)',
+        message: 'Please provide steps necessary to install your project.',
     },
     {
         type: 'input',
         name: 'usage',
-        message: 'Please provide instructions for use and examples thereof. (Required)',
+        message: 'Please provide instructions for use and examples thereof.',
     },
     {
         type: 'input',
         name: 'contributions',
-        message: 'Please provide instructions for how other developers can contribute to your project. (Required)',
+        message: 'Please provide instructions for how other developers can contribute to your project.',
     },
     {
         type: 'input',
         name: 'tests',
-        message: 'Please provide examples of how tests for your project can be run. (Required)',
+        message: 'Please provide examples of how tests for your project can be run.',
     },
     {
         type: 'list',
         name: 'license',
-        message: 'Please select which license applies to your project. For help selecting a license, please checkout [https://choosealicense.com/]. (Required)',
+        message: 'Please select which license applies to your project. For help selecting a license, please checkout [https://choosealicense.com/].',
         choices: ['MIT', 'ISC', 'Apache License 2.0', 'GNU GPLv3', 'CC0-1.0', 'CC-BY-4.0', 'Boost Software License 1.0', 'I choose not to license my project'],
-              /*       validate: licenseInput => {
-                        if (licenseInput) {
-                            return true;
-                        } else {
-                            console.log("Please select one of the listed options!");
-                            return false;
-                        }
-                    } */
 },
 {
     type: 'input',
         name: 'github',
-            message: 'Please provide your GitHub username. (Required)',
-         /*        validate: usernameInput => {
-                    if (usernameInput) {
-                        return true;
-                    } else {
-                        console.log("Please provide your GitHub username!");
-                        return false;
-                    }
-                } */
+            message: 'Please provide your GitHub username.',
 },
 {
     type: 'input',
         name: 'email',
-            message: 'Please provide your email address for users to reach out with additional questions. (Required)',
-               /*  validate: emailInput => {
-                    if (emailInput) {
-                        return true;
-                    } else {
-                        console.log("Please provide your email address!");
-                        return false;
-                    }
-                } */
+            message: 'Please provide your email address for users to reach out with additional questions.',
 }
 ];
 
